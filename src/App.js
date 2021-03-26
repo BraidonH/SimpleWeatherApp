a78a777b5108e6b88ec5c65fce95cd8646ef30df
BraidonH <braidonhjames@icloud.com>
2021-03-26 15:40:29 -0500

    weatherapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Inputs from './inputOptions/inputs'
+import  {useEffect} from 'react'
 
 function App() {
+
+  useEffect(() => {
+    const d = new Date();
+    const month = d.getMonth() + 1;
+    const day = d.getDate();
+    const display = `${month}/${day}`
+    const ele = document.getElementById("displayDate")
+    ele.innerHTML = display
+  }, []);
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="displayDate">Display Date</h1>
+      <header id="header">
+        <h1>WEATHER APP</h1>
+      </header> 
+
+      <div id="inputs">
+        <Inputs />
+      </div>
     </div>
+
   );
 }
 
---
+++ src/inputOptions/inputs.js
@@ -0,0 +1,89 @@
+import {useState, useEffect} from 'react'
+
+
+
+
+
+
+export default function Inputs()  {
+    const [values, setValues] = useState({city:"", state:""})
+    const [weather, setWeather] = useState({})
+
+   
+    
+        
+
+
+    const handleChange = event => {
+        const {name, value } = event.target;
+           setValues({
+               ...values,
+               [name]: value
+           })
+      }
+
+    function apiCall() {
+      const YOUR_ACCESS_KEY = "24cf2be62f50db798e80fd85dd76d12d"
+      fetch(`http://api.weatherstack.com/current?access_key=${YOUR_ACCESS_KEY}&query=${values.city},${values.state}`)
+      .then(res => res.json()).then(data => {
+           setWeather(data.current)
+      })
+    }
+
+    document.onkeydown = function (e) {
+        e = e || window.event;
+        switch (e.which || e.keyCode) {
+              case 13 :
+                  apiCall()
+                  break;
+        }
+      }
+ 
+
+    return (
+    <div>    
+        <div classname="city">
+            <input name="city" type="text" value={values.city} onChange={handleChange} placeholder="city"/>
+        </div>
+        <div classname="state">
+            <input name="state" type="text" value={values.state} onChange={handleChange} placeholder="state"/>
+        </div>
+        <button onClick={apiCall}>search</button>
+        
+
+
+
+
+        <section id="section">
+           <div id="weathercontainer">
+               <label>Temperature</label>
+               <h1 id="weather">{weather.temperature}<supp>c</supp></h1>
+           </div>
+           <div id="weathercontainer">
+               <label>Feels Like</label>
+               <h1 id="weather">{weather.feelslike}<supp>c</supp></h1>
+           </div>
+           <div id="weathercontainer">
+               <label>Humdity</label>
+               <h1 id="weather">{weather.humidity}<supp>%</supp></h1>
+           </div>
+           <div id="weathercontainer">
+               <label>Precip</label>
+               <h1 id="weather">{weather.precip}<supp>%</supp></h1>
+           </div>
+           <div id="weathercontainer">
+               <label>Wind</label>
+               <h1 id="weather">{weather.wind_speed}<supp></supp> {weather.wind_dir}</h1>
+           </div>
+           <div id="weathercontainer">
+               <label>visibility</label>
+               <h1 id="weather">{weather.visibility}<supp>ft</supp></h1>
+           </div>
+        </section>
+        <article>
+            <p>{weather.observation_time}</p>
+            <p>{weather.weather_descriptions}</p>
+        </article>
+    </div>
+    )
+}
